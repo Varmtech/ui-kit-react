7365be779d4ebcb100dd8e89c4dfe156afd61d2f
armensct <armen@sceyt.com>
2023-05-17 20:07:07 +0400

    change window to global on checking caches

--- index.js
+++ index.js
@@ -20580,7 +20580,7 @@ var getFrame = function getFrame(videoSrc, time) {
 };
 
 var ATTACHMENTS_CACHE = 'attachments-cache';
-var cacheAvailable = ('caches' in window);
+var cacheAvailable = ('caches' in global);
 var setAttachmentToCache = function setAttachmentToCache(attachmentId, attachmentResponse) {
   if (cacheAvailable) {
     caches.open(ATTACHMENTS_CACHE).then(function (cache) {
--- index.modern.js
+++ index.modern.js
@@ -20576,7 +20576,7 @@ var getFrame = function getFrame(videoSrc, time) {
 };
 
 var ATTACHMENTS_CACHE = 'attachments-cache';
-var cacheAvailable = ('caches' in window);
+var cacheAvailable = ('caches' in global);
 var setAttachmentToCache = function setAttachmentToCache(attachmentId, attachmentResponse) {
   if (cacheAvailable) {
     caches.open(ATTACHMENTS_CACHE).then(function (cache) {
